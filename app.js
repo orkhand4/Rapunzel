36c4b0959e2ead70efa9a25608b2d97c9b9c7df1
Orkhand4 <orkhand4@mail.ru>
2024-09-28 11:09:50 +0400

    fakestoreapi async

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const cardsWrapper = document.querySelector(".cards-wrapper");
 
-fetch('https://fakestoreapi.com/products')
+const getData = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
 
-.then((res) => res.json())
-.then((products) => {
-  products.map((item) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `<img src="${item.image}" alt="">
-          <h2>${item.title.slice(0,10)}...</h2>
+  const products = await response.json();
+
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((products) => {
+      products.map((item) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `<img src="${item.image}" alt="">
+          <h2>${item.title.slice(0, 10)}...</h2>
           <p>${item.category}</p>
-          <button>Rate:${item.rating.rate} - Stock: ${item.rating.count}</button>`;
-  
-    cardsWrapper.appendChild(card);
-  
-  });
+          <button>Rate:${item.rating.rate} - Stock: ${
+          item.rating.count
+        }</button>`;
 
-});
+        cardsWrapper.appendChild(card);
+      });
+    });
+};
 
+getData()
